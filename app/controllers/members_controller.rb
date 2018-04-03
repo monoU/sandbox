57f617a3a614cfdba4126dd2264c3e2335e3a4e8
monoU <>
2018-04-03 18:21:01 +0900

    member定義削除

--- app/controllers/members_controller.rb
+++
@@ -1,9 +0,0 @@
-class MembersController < InheritedResources::Base
-
-  private
-
-    def member_params
-      params.require(:member).permit(:name, :user_name, :age, :address, :email, :phone)
-    end
-end
-
